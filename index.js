ada406106169e12921c967bbe81c510f8e0e1f6d
Christian <xtianluz@gmail.com>
2021-11-01 22:10:21 +0800

    made uploads static

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const multer = require('multer')
 
+//express.static make the folder path '/uploads'+filename available in the browser
 app.use('/uploads', express.static('uploads'))
 
 const storage = multer.diskStorage({
@@ -23,11 +24,11 @@ const fileFilter = (req,file,cb) => {
 
 const upload = multer({
     storage: storage,
-    limites: {
+    limits: {
         fileSize: 1024 * 1024 * 6,
     },
     fileFilter: fileFilter
-});
+}).single('img');
 
 let users = [
     {
@@ -42,9 +43,32 @@ let users = [
     }
 ]
 
-app.post('/uploads', upload.single("img"), (req, res) => {
-    console.log(req.file)
-})
+// simplest form here
+// app.post('/uploads', upload.single("img"), (req, res) => {
+//     console.log(req.file)
+//     res.send('Upload successful!')
+
+// })
+
+
+//complex form
+app.post('/uploads', function (req, res) {
+    upload(req, res, function (err) {
+      if (err instanceof multer.MulterError) {
+        // A Multer error occurred when uploading.
+        console.log(err)
+        res.send(err)
+      } else if (err) {
+        // An unknown error occurred when uploading.
+        res.send('Unknown error!')
+      }
+  
+      // Everything went fine.
+      res.send('Upload successful!')
+      console.log(req.file)
+    })
+  })
+
 
 app.get('/',(req, res) => {
     res.send('<h1>Coming soon...</h1>')
