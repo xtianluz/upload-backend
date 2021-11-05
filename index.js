33bc264ac79dbf6a28118fafad9ec9a02c49ce21
Christian <xtianluz@gmail.com>
2021-11-06 01:31:20 +0800

    upload works on front end and saved to mongodb

--- index.js
+++ index.js
@@ -2,13 +2,26 @@ const express = require('express')
 const app = express()
 const multer = require('multer')
 const cors = require('cors')
-
+const mongoose = require('mongoose')
 
 app.use(cors())
 app.use(express.json())
 
 //express.static make the folder path '/uploads'+filename available in the browser
 app.use('/uploads', express.static('uploads'))
+
+//--------------------------------------------------mongoose block
+mongoose.connect('mongodb://localhost:27017/myFirstDB')
+.then(() => {
+    console.log('Mongodb connected...')
+})
+
+const uriSchema = new mongoose.Schema({
+    imageUri: String,
+});
+
+const seller = mongoose.model('seller', uriSchema)
+//--------------------------------------------------mongoose block
 
 
 const storage = multer.diskStorage({
@@ -46,30 +59,42 @@ const upload = multer({
 
 // })
 
+
+/*IMAGE UPLOAD BLOCK*******************************************/
 //complex form
 app.post('/uploads', function (req, res) {
     upload(req, res, function (err) {
       if (err instanceof multer.MulterError) {
-        // A Multer error occurred when uploading.
         console.log(err)
         res.send(err)
       } else if (err) {
-        // An unknown error occurred when uploading.
         res.send('Unknown error!')
       }
   
       // Everything went fine.
+      const sellerUpload = new seller({
+        imageUri: "http://localhost:5000/" + req.file.path.toString()
+        })
+      sellerUpload.save()
+      .then((result) => {
+          console.log('Seller upload saved!')
+          mongoose.connection.close()
+          console.log('Mongodb closed!')
+      })
       res.send(req.file)
-      console.log(req.file)
+      console.log(req.file.path)
     })
 })
+/*IMAGE UPLOAD BLOCK*******************************************/
 
 app.get('/',(req, res) => {
     res.send('<h1>Coming soon...</h1>')
 })
 
-app.get('/api/users', (req, res) => {
-    res.json([])
+app.get('/uploads', (req, res) => {
+    seller.find({}).then(items => {
+        res.json(items)
+    })
 })
 
 const PORT = 5000
